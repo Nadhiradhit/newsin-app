985349d17331426a38b27abf77194ed1c88c1ba9
Nadhiradhit <nadhiradhitya@gmail.com>
2023-10-10 22:13:51 +0700

    feat: add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,26 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+      mytheme: {
+        "primary": "#04364A",
+        "secondary": "#176B87",
+        "accent": "#64CCC5",
+        "neutral": "#DAFFFB",   
+        "base-100": "#ffffff", 
+        "info": "#3abff8",   
+        "success": "#36d399",   
+        "warning": "#fbbd23",
+        "error": "#f87272",
+      },
+      mydark:{
+        
+      }
+    }
+  ]
+  },
   plugins: [
     require("daisyui"),
   ],
