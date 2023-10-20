0b0bfe9fc2d8883c4e95f7c7c84191847a6f8769
Nadhiradhit <nadhiradhitya@gmail.com>
2023-10-20 11:14:44 +0700

    feat: paginate view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Session;
 
 class PostController extends Controller
 {
@@ -14,7 +16,8 @@ class PostController extends Controller
 
         // Read Database Post
         $user = User::all();
-        $post = Post::with('category')->get();
+        $post = Post::simplePaginate(5);
+        // $post = Post::with('category')->get();
         // dd($post);
         return view("admin.index", ["postData" => $post, "userData" => $user], compact("title"));
     }
@@ -23,7 +26,7 @@ class PostController extends Controller
         
         $title = "Dashboard Admin | News";
 
-        $category = Category::select('id', 'name')->get();
+        $category = Category::select("id", "name")->get();
 
         return view("admin.create", ["categoryData" => $category],compact("title"));
     }
@@ -36,8 +39,13 @@ class PostController extends Controller
         $post->author = $request->author;
         $post->description_news = $request->description_news;
         $post->content = $request->content;
-
         $post->save();
+
+        if($post) {
+            Session::flash("status", "success");
+            Session::flash("message", "Data Berhasil Terinput");
+        }
+        
 
         return redirect()->route("admin.create");
 
@@ -47,17 +55,48 @@ class PostController extends Controller
 
     public function show($id){
         $title = "Dashboard Admin | Detail";
-        $post = Post::with(['category'])
+        $post = Post::with(["category"])
             ->findOrFail($id);
 
-        return view("admin.post.detail", ["post" => $post],compact("title"));
+        return view("admin.post.detail", ["post" => $post], compact("title"));
     }
 
-    public function delete($id){
+    public function edit(Request $request, $id){
+        $title = "Dashboard Admin | Edit";
+        $post = Post::with(["category"])->findOrFail($id);
+        $category = Category::where("id", "!=", $post->category_id)->get(["id", "name"]);
+        // dd($post);
+        return view("admin.edit", ["post" => $post, "category" => $category], compact("title"));
+    }
+
+    public function update(Request $request, $id){
         $post = Post::findOrFail($id);
-        return view("admin.delete", ['post' => $post]);
+
+        $post->header = $request->header;
+        $post->category_id = $request->category_id;
+        $post->author = $request->author;
+        $post->description_news = $request->description_news;
+        $post->content = $request->content;
+        $post->save();
+
+        return redirect("/news");
+        
+    }
+
+    public function destroy($id){
+
+        $deletePost = Post::findOrFail($id);
+        $deletePost ->delete();
+
+        if($deletePost) {
+            Session::flash("status", "success");
+            Session::flash("message", "Data Berhasil Dihapus");
+        }
+
+        return redirect("/news");
     }
     
+
 
 
 }
